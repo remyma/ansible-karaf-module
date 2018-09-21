029f677d14e97baee6526977f6281781e7985653
Christoffer Lind <christoffer.lind@yandex.com>
2018-09-21 23:18:24 +0200

    Added a basic karaf_config module

---
+++ karaf_config.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import *
+
+PACKAGE_STATE_MAP = dict(
+    present="property-set",
+    absent="property-delete"
+)
+
+_BOOL_TYPES = frozenset(['true', 'false', 'yes', 'no', 'y', 'n'])
+
+def check_bool(value):
+    if not value.lower() in _BOOL_TYPES:
+        raise ValueError()
+    
+    v = value.lower()
+    return  v == 'true' or \
+            v == 'yes' or \
+            v == 'y'
+    
+def convert(val):
+    constructors = [int, float, check_bool, str]
+    for c in constructors:
+        try:
+            return c(val);
+        except ValueError:
+            pass
+
+def existing_properties(module, client_bin, name, new_properties):
+    cmd_base = 'config:property-get --raw --pid %s %s'
+    karaf_cmd = ' && '.join([cmd_base % (name, prop) for prop in new_properties.keys()])
+    
+    rc, out, err = module.run_command('%s "%s"' % (client_bin, karaf_cmd))
+    lines = out.split('\n')
+    result = {}
+    
+    for line in lines:
+        if '=' not in line:
+            continue
+        
+        i = line.find('=')
+        prop_name = line[:i].strip()
+        value = convert(line[i+1:].strip())
+            
+        result[prop_name] = value
+    
+    return result
+
+def config_property_set(client_bin, module, name, new_properties):
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+
+    existing_props = existing_properties(module, client_bin, name, new_properties)
+    need_change = [k for k,v in new_properties.items() if k not in existing_props or existing_props[k] != v]
+        
+    if not need_change:
+        return result
+    
+    result['changed'] = True
+    if module.check_mode:
+        return result
+    
+#     result['diff'] = {
+#         "before": "hello",
+#         "after": "goodbye"
+#         }
+#     
+    cmd_base = 'config:property-set --pid "%s" %s %s'
+    cmd = ' && '.join([ cmd_base % (name, k, v) for k,v in new_properties.items() if k in need_change])
+    rc, out, err = module.run_command('%s "%s"' % (client_bin, cmd))
+    
+    return result
+
+def config_property_delete(client_bin, module, name, properties):
+    result = dict(
+        changed=False,
+        original_message='',
+        message=''
+    )
+    
+    existing_props = existing_properties(module, client_bin, name, properties)
+    
+    need_delete = [k for k in properties.keys() if k in existing_props]
+    
+    if not need_delete:
+        return result
+    
+    result['changed'] = True
+    if module.check_mode:
+        return result
+    
+    cmd_base = 'config:property-delete --pid "%s" %s'
+    cmd = ' && '.join([ cmd_base % (name, k) for k in properties.keys() if k in need_delete])
+    rc, out, err = module.run_command('%s "%s"' % (client_bin, cmd))
+    return result
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            name=dict(required=True),
+            state=dict(default="present", choices=PACKAGE_STATE_MAP.keys()),
+            properties=dict(required=True, type="dict"),
+            client_bin=dict(default="/opt/karaf/bin/client", type="path")
+        ),
+        supports_check_mode=True
+    )
+
+    name = module.params["name"]
+    state = module.params["state"]
+    client_bin = module.params["client_bin"]
+    properties = module.params["properties"]
+    
+    if state == "present":
+        result = config_property_set(client_bin, module, name, properties)
+    elif state == "absent":
+        result = config_property_delete(client_bin, module, name, properties)
+
+    module.exit_json(**result)
+
+if __name__ == '__main__':
+    main()
